8d5800655ade9d9c9a07686f27e58394ca2e9ed0
akhilkrishnair <akhilkrishna36912@gmail.com>
2023-08-13 15:05:00 +0530

    navbar designed

---
+++ personal-website/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './css/Navbar.css'
+const Navbar = () => {
+  return (
+    <>
+    <div className='nav-container'>
+        
+        <div className='my-name'><div className='rounded'></div><span className='letter-a'>A</span>khil <span className='letter-a'>K</span>rishna <span className='role'> Developer</span></div>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>|</a></li>
+
+            <li><a>Projects</a></li>
+            <li><a>|</a></li>
+
+            <li><a>Contact me</a></li>
+
+        </ul>
+    </div>
+    
+    </>
+  )
+}
+
+export default Navbar
