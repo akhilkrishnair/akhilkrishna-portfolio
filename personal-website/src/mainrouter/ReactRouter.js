7c85b3939b2bbb59b134e0dc833ce3744dfd0d31
akhilkrishnair <akhilkrishna36912@gmail.com>
2023-08-27 21:06:55 +0530

    initial commit

--- personal-website/src/mainrouter/ReactRouter.js
+++ personal-website/src/mainrouter/ReactRouter.js
@@ -5,6 +5,7 @@ import Footer from "./ReactRouters/componets/Footer";
 import Navbar from "./ReactRouters/componets/Navbar";
 import Projects from "./ReactRouters/componets/Projects";
 import Contact from "./ReactRouters/componets/Contact";
+import Otherpages from "./ReactRouters/componets/Otherpages";
 
 
 const ReactRouter = () => {
@@ -15,6 +16,7 @@ const ReactRouter = () => {
                 <Route path="/" element= {<Home/>} />
                 <Route path="projects" element = {<Projects/>} />
                 <Route path="contact" element = {<Contact/>} />
+                <Route path="*" element = {<Otherpages/>} />
             </Routes>
             <Footer/>
         </div>
